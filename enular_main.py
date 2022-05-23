8bf6c80ef07dd792adb4e76c66d66bf5c45453f6
adriancpwong <adriancpwong@hotmail.com>
2022-05-23 14:26:11 +0100

    Initialise

---
+++ enular_main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from import_test import *
+
+if __name__ == '__main__':
+    print ('Do not run this file.')
+
+class Test:
+    
+    a = 1
+    b = 2
+
+    def __init__(self):
+        self.b = 3
+    
+    def printy(self):
+        print(self.b)
+        test_function()
--- hello.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import os
-import sys
-
-
-print("hello world")
