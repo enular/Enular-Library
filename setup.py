a0e339a9308be29bd61fdc31bdf939f11c028048
adriancpwong <adriancpwong@hotmail.com>
2022-07-05 15:18:36 +0100

    Updates

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="enular",
-    version="0.1.5",
+    version="0.1.6",
     description="Enular Library",
     long_description=long_description,
     long_description_content_type="text/markdown",
