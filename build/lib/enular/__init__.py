d09432ab64b44daced9009896a3cde6ffc107559
adriancpwong <adriancpwong@hotmail.com>
2022-07-05 15:20:03 +0100

    Removed files

--- build/lib/enular/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import *
-from .analyzers import *
-
-from . import indicators as indicators
-from . import strategies as strategies
-
-
--- build/lib/enular/enular_train.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-import sys
-
-import backtrader as bt
-from enular.enular_base import *
-
-class Train(Analyzer):
-    pass
